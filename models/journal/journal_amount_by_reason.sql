77918d0ef3e3a03edbf01e33653a8d63716f7ac4
Xinxin Dai <xinxin.dai@chime.com>
2022-07-20 16:36:22 -0700

    add tests and first journal model

---
+++ models/journal/journal_amount_by_reason.sql
@@ -0,0 +1,6 @@
+SELECT
+    reason,
+    SUM(IFF(amount < 0, ABS(amount), 0)) as debit,
+    SUM(IFF(amount >= 0, ABS(amount), 0)) as credit
+FROM {{ ref('journal') }}
+GROUP BY reason
---
+++ tests/generic/not_negative.sql
@@ -0,0 +1,8 @@
+{% test not_negative(model, column_name) %}
+
+select
+    {{ column_name }}
+from {{ model }}
+where {{ column_name }} < 0
+
+{% endtest %}
