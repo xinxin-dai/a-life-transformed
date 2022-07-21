8719b992c9738a7a3165aa9b236d753d2dbec6d7
ANANYA RAJAGOPAL <ananya.rajagopal@chime.com>
2022-07-21 13:56:28 -0700

    Added ledger daily amount by reason file

---
+++ models/ledger/ledger_daily_amount_by_reason.sql
@@ -0,0 +1,15 @@
+/*
+    Aggregate ledger transactions by date and reason,
+   
+
+*/
+
+ SELECT
+    date(date) as day,
+    reason,
+    account,
+    SUM(credit) as credit,
+    SUM(debit) as debit
+FROM {{ ref('ledger') }}
+GROUP BY day, reason, account
+
