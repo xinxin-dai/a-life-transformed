008251f8489ed5e4ae5ad84cdc48ed52703db9b1
Xinxin Dai <xinxin.dai@chime.com>
2022-07-22 10:35:00 -0700

    turn ending_balance into a incremental model and add documentations

--- models/ledger/ledger_stride_daily_ending_balance.sql
+++ models/ledger/ledger_stride_daily_ending_balance.sql
@@ -1,8 +1,32 @@
 
-select
-    day as date, account,
-    SUM(COALESCE(credit,0) - COALESCE(debit,0)) as balance
-from {{ ref('ledger_daily_amount_by_reason') }}
-where account = 'Stride Activity'
-group by date, account
+{{
+    config(
+        materialized='incremental',
+        unique_key=['date', 'account']
+    )
+}}
 
+WITH prev AS (
+    SELECT
+        account,
+        balance
+    FROM {{ this }}
+    WHERE account = 'Stride Activity'
+    AND date = DATEADD(day, -1, DATE('{{ var("snapshot_date") }}'))
+),
+curr AS (
+    select
+        account,
+        SUM(COALESCE(credit,0) - COALESCE(debit,0)) as amount
+    from {{ ref('ledger_daily_amount_by_reason') }}
+    where account = 'Stride Activity'
+    and day = DATE('{{ var("snapshot_date") }}')
+    group by account
+)
+
+SELECT
+    DATE('{{ var("snapshot_date") }}') AS date,
+    COALESCE(p.account, c.account) AS account,
+    (COALESCE(p.balance, 0) + COALESCE(c.amount, 0)) AS balance
+FROM prev p
+FULL OUTER JOIN curr c ON p.account = c.account
---
+++ models/models.md
@@ -0,0 +1,6 @@
+{% docs ledger_stride_daily_ending_balance %}
+
+This table contains the daily ending balance of these back office accounts:
+- Stride Activity
+
+{% enddocs %}
